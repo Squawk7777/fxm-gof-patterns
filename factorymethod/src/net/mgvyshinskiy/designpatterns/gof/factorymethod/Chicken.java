c7c10203056a6d3b14ba13ec052697a00d2a3a54
Mikhail Vyshinskiy <mg.vyshinskiy@gmail.com>
2020-01-20 14:00:47 +0300

    Factory method implementation

---
+++ factorymethod/src/net/mgvyshinskiy/designpatterns/gof/factorymethod/Chicken.java
@@ -0,0 +1,8 @@
+package net.mgvyshinskiy.designpatterns.gof.factorymethod;
+
+public class Chicken implements Meal {
+    @Override
+    public String toString() {
+        return "Here is the chicken! Hot as hell!";
+    }
+}
---
+++ factorymethod/src/net/mgvyshinskiy/designpatterns/gof/factorymethod/Meal.java
@@ -0,0 +1,4 @@
+package net.mgvyshinskiy.designpatterns.gof.factorymethod;
+
+public interface Meal {
+}
